0f9b8468ca9b738fa57b90484819921dfbc386e2
Akshat Jain <145150367+AkshatInnovates@users.noreply.github.com>
2025-08-29 06:36:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"Use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+// code readability should be high
+
+let name = "Akshat"
+let age = 18
+let isLoggedIn = true
+let state;
+let city = null
+
+// number => 2 to pwr 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value will not assined
+// symbol => unique
+
+// object
+
+console.log(typeof "akshat");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
